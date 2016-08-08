d673529ea39db8093f3f539b23bd5b695f4f61fe
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-08 01:39:51 +0000

    Add necessary JS for Stripe integration

---
+++ app/assets/javascripts/stripe.js
@@ -0,0 +1,52 @@
+$(function() {
+   
+  var show_error, stripeResponseHandler, submitHandler;
+  
+  $('.cc-form').on('submit', submitHandler);
+  
+  submitHandler = function(e) {
+    var $form = $(e.target);
+    $form.find('input[type=submit]').prop('disabled', true);
+    
+    if ( Stripe ) {
+      Stripe.card.createToken($form, stripeResponseHandler);
+    } else {
+      show_error('Failed to load credit card processing functionality. Please reload this page in your browser.')
+    }
+    
+    return false;
+  }
+  
+  stripeResponseHandler = function(status, response) {
+    var token, $form;
+    
+    $form = $('.cc-form');
+    
+    if ( response.error ) {
+      console.log(response.error.message);
+      show_error(response.error.message);
+      $form.find('input[type=submit]').prop('disabled', false);
+    } else {
+      token = response.id;
+      $form.append('<input type="hidden" name="payment[token]">').val(token);
+      $('[data-stripe=number').remove();
+      $('[data-stripe=cvv').remove();
+      $('[data-stripe=exp-month').remove();
+      $('[data-stripe=exp-year').remove();
+      $form.get(0).submit();
+    }
+    
+    return false;
+  }
+  
+  show_error = function(message) {
+    if ( $('#flash-messages').size < 1 ) {
+      $('.container.main div:first').prepend('<div id="flash-messages"></div>');
+    }
+    
+    $('#flash-messages').html('<div class="alert alert-warning"><a class="close" data-dismiss="alert">x</a><div id="flash_alert">' + message + '</div></div>');
+    $('.alert').delay(5000).fadeOut(3000);
+    return false;
+  }
+    
+});
