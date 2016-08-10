73eddca823d1928bfc93f50163c0577659d33ad1
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-10 01:16:08 +0000

    Add validations for image uploads

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,13 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validates :picture_size
+  
+  private
+  
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "must be less than 5MB")
+      end
+    end
 end
